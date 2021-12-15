d625e0b430a09a9893b4c70e225948f1c255f5b2
Joker Zhong <jiahaozhongjoker@gmail.com>
2021-12-15 18:11:49 -0500

    Update login.php

--- SHS/login/login.php
+++ SHS/login/login.php
@@ -9,7 +9,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 }
  
 // Include file for connection
-include "connection.php";
+include 'connectdb.php';
  
 // Define variables and initialize with empty values
 $username = $password = "";
