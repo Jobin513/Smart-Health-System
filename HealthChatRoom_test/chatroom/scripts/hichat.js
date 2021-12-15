c7d7a8a09981175dca575e36c1ae37244c4b3889
JacenWang <70711722+jiachenwa@users.noreply.github.com>
2021-12-14 20:29:50 -0500

    Add files via upload

---
+++ HealthChatRoom_test/chatroom/scripts/hichat.js
@@ -0,0 +1,132 @@
+/*
+ *
+ *Wayou Mar 28,2014
+ *MIT license
+ *view on GitHub:https://github.com/wayou/HiChat
+ */
+
+window.onload = function() {
+    var healthChat = new HealthChat();
+    healthChat.init();
+};
+var HealthChat = function() {
+    this.socket = null;
+};
+HealthChat.prototype = {
+    init: function() {
+        var that = this;
+        this.socket = io.connect();
+        this.socket.on('connect', function() {
+            document.getElementById('info').textContent = 'get yourself a nickname :)';
+            document.getElementById('nickWrapper').style.display = 'block';
+            document.getElementById('nicknameInput').focus();
+        });
+        this.socket.on('nickExisted', function() {
+            document.getElementById('info').textContent = '!nickname is taken, choose another pls';
+        });
+        this.socket.on('loginSuccess', function() {
+            document.title = 'healthChat | ' + document.getElementById('nicknameInput').value;
+            document.getElementById('loginWrapper').style.display = 'none';
+            document.getElementById('messageInput').focus();
+        });
+        this.socket.on('error', function(err) {
+            if (document.getElementById('loginWrapper').style.display == 'none') {
+                document.getElementById('status').textContent = '!fail to connect :(';
+            } else {
+                document.getElementById('info').textContent = '!fail to connect :(';
+            }
+        });
+        this.socket.on('system', function(nickName, userCount, type) {
+            var msg = nickName + (type == 'login' ? ' joined' : ' left');
+            that._displayNewMsg('system ', msg, 'red');
+            document.getElementById('status').textContent = userCount + (userCount > 1 ? ' users' : ' user') + ' online';
+        });
+        this.socket.on('newMsg', function(user, msg, color) {
+            that._displayNewMsg(user, msg, color);
+        });
+        this.socket.on('newImg', function(user, img, color) {
+            that._displayImage(user, img, color);
+        });
+        document.getElementById('loginBtn').addEventListener('click', function() {
+            var nickName = document.getElementById('nicknameInput').value;
+            if (nickName.trim().length != 0) {
+                that.socket.emit('login', nickName);
+            } else {
+                document.getElementById('nicknameInput').focus();
+            };
+        }, false);
+        document.getElementById('nicknameInput').addEventListener('keyup', function(e) {
+            if (e.keyCode == 13) {
+                var nickName = document.getElementById('nicknameInput').value;
+                if (nickName.trim().length != 0) {
+                    that.socket.emit('login', nickName);
+                };
+            };
+        }, false);
+        document.getElementById('sendBtn').addEventListener('click', function() {
+            var messageInput = document.getElementById('messageInput'),
+                msg = messageInput.value,
+                color = document.getElementById('colorStyle').value;
+            messageInput.value = '';
+            messageInput.focus();
+            if (msg.trim().length != 0) {
+                that.socket.emit('postMsg', msg, color);
+                that._displayNewMsg('me', msg, color);
+                return;
+            };
+        }, false);
+        document.getElementById('messageInput').addEventListener('keyup', function(e) {
+            var messageInput = document.getElementById('messageInput'),
+                msg = messageInput.value,
+                color = document.getElementById('colorStyle').value;
+            if (e.keyCode == 13 && msg.trim().length != 0) {
+                messageInput.value = '';
+                that.socket.emit('postMsg', msg, color);
+                that._displayNewMsg('me', msg, color);
+            };
+        }, false);
+       
+        document.getElementById('sendImage').addEventListener('change', function() {
+            if (this.files.length != 0) {
+                var file = this.files[0],
+                    reader = new FileReader(),
+                    color = document.getElementById('colorStyle').value;
+                if (!reader) {
+                    that._displayNewMsg('system', '!your browser doesn\'t support fileReader', 'red');
+                    this.value = '';
+                    return;
+                };
+                reader.onload = function(e) {
+                    this.value = '';
+                    that.socket.emit('img', e.target.result, color);
+                    that._displayImage('me', e.target.result, color);
+                };
+                reader.readAsDataURL(file);
+            };
+        }, false);
+        
+       
+        
+       
+    },
+  
+    _displayNewMsg: function(user, msg, color) {
+        var container = document.getElementById('historyMsg'),
+            msgToDisplay = document.createElement('p'),
+            date = new Date().toTimeString().substr(0, 8);
+            
+        msgToDisplay.style.color = color || '#000';
+        msgToDisplay.innerHTML = user + '<span class="timespan">(' + date + '): </span>' + msg;
+        container.appendChild(msgToDisplay);
+        container.scrollTop = container.scrollHeight;
+    },
+    _displayImage: function(user, imgData, color) {
+        var container = document.getElementById('historyMsg'),
+            msgToDisplay = document.createElement('p'),
+            date = new Date().toTimeString().substr(0, 8);
+        msgToDisplay.style.color = color || '#000';
+        msgToDisplay.innerHTML = user + '<span class="timespan">(' + date + '): </span> <br/>' + '<a href="' + imgData + '" target="_blank"><img src="' + imgData + '"/></a>';
+        container.appendChild(msgToDisplay);
+        container.scrollTop = container.scrollHeight;
+    }
+};
